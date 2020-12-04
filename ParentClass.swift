43550dbae0c2fd25ad3a649c13c5c840f318ed4f
Gunjan-ios <20123906+Gunjan-ios@users.noreply.github.com>
2020-12-04 17:32:47 +0530

    work

---
+++ ParentClass.swift
@@ -0,0 +1,269 @@
+//
+//  ParentViewController.swift
+//  GEM
+//
+//  Created by Gunjan Raval on 8/23/18.
+//  Copyright © 2018 Gunjan Raval. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class ParentClass: UIViewController{
+   
+    static let shared = ParentClass()
+
+    // ----------------------------------------------------------
+    // MARK: Private Members
+    // ----------------------------------------------------------
+    
+    var APP = UIApplication.shared.delegate as! AppDelegate
+    var window: UIWindow?
+    var iPhone_SE:Bool = false
+    var iPhone_X_Top_Padding:CGFloat = 0
+    var iPhone_X_Bottom_Padding:CGFloat = 0
+    var token : String!
+    var saveListArray : [[String : Any]] = [[String:Any]]()
+    var saveListArray1 : [JSON] = [JSON]()
+    var lblSubTitle : UILabel!
+//    var mainData : DiaryData!
+    var mainData : [DiaryData] = [DiaryData]()
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .black
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+//        self.navigationController?.isNavigationBarHidden = true
+        let imgNav = UIView (frame: CGRect (x: 0, y: 0, width: SCREEN_WIDTH, height: STATUS_BAR_HEIGHT))
+        imgNav.backgroundColor = colorPrimary
+        self.view.addSubview(imgNav)
+        
+
+        self.lblSubTitle = UILabel (frame: CGRect (x: X_PADDING, y: 0, width: SCREEN_WIDTH - X_PADDING*2, height: SCREEN_HEIGHT))
+        //            lblSubTitle.center = CGPoint (x: self.view.center.x, y: lblSubTitle.center.y)
+        self.lblSubTitle.text =  CS.Common.NoData
+        self.lblSubTitle.numberOfLines = 0
+        self.lblSubTitle.lineBreakMode = .byWordWrapping
+        self.lblSubTitle.textAlignment = .center
+        self.lblSubTitle.font = UIFont (name: APP_FONT_NAME_BOLD, size: HEADER_FONT_SIZE)
+        self.lblSubTitle.textColor = .white
+        self.lblSubTitle.isHidden = true
+        self.view.addSubview(self.lblSubTitle)
+    }
+    
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    func setData(strData:Any,strKey:String) {
+        
+        UserDefaults.standard.set(strData, forKey: strKey)
+        UserDefaults.standard.synchronize()
+    }
+    
+    func getDataForKey(strKey:String) -> Any {
+        return UserDefaults.standard.value(forKey: strKey) as Any
+    }
+    
+    func detectScreenShot(action: @escaping () -> ()) {
+        let mainQueue = OperationQueue.main
+        NotificationCenter.default.addObserver(forName: UIApplication.userDidTakeScreenshotNotification, object: nil, queue: mainQueue) { notification in
+            // executes after screenshot
+            action()
+        }
+    }
+    
+    func getDate(date:String)-> String {
+        if (date == ""){
+            return date
+        }
+        let splitdate : [Substring] = date.split(separator: ".")
+        let subString = splitdate.first!
+        let newDate = String(subString)
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatterGet.timeZone = TimeZone(abbreviation: "UTC")
+        
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "dd/MM/yyyy"
+        
+        if let date1 = dateFormatterGet.date(from:newDate ){
+            dateFormatterPrint.timeZone = TimeZone.current
+            dateFormatterPrint.timeStyle = .medium //Set time style
+            dateFormatterPrint.dateStyle = .medium //Set date style
+//            dateFormatterPrint.dateFormat = "yyyy-MM-dd  HH:mm:ss"
+            return dateFormatterPrint.string(from:date1)
+        }
+        else {
+            print("There was an error decoding the string")
+        }
+        return date
+    }
+    func dateConvert(date: String?) -> Date{
+        
+//        let isoDate = date
+//print(isoDate)
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.locale = Locale(identifier: "en_US_POSIX") // set locale to reliable US_POSIX
+//        dateFormatter.dateFormat = "yyyy-MM-dd"
+//        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:SSSZ"
+//
+//        let date = dateFormatter.date(from:isoDate!)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        let date = dateFormatter.date(from: date!)
+        print("date: \(date)")
+        
+        return date!
+        
+//        if let timeResult = date {
+//            let date = NSDate(timeIntervalSince1970: timeResult)
+//            let dateFormatter = DateFormatter()
+//            dateFormatter.timeStyle = .medium //Set time style
+//            dateFormatter.dateStyle = .medium //Set date style
+//            let localDate = dateFormatter.string(from: date as Date)
+//            return localDate
+//        }else{
+//            return ""
+//        }
+    }
+    func getDataJSON(key:String) -> [DiaryData] {
+        if let listArray = self.getDataForKey(strKey: key) as? Data {
+            if let decodedArray = NSKeyedUnarchiver.unarchiveObject(with: listArray) as? [DiaryData] {
+                return decodedArray
+            }
+        }
+        return [DiaryData]()
+    }
+
+    func setJSON(json: [DiaryData], key:String)  {
+        let encodedData: Data = NSKeyedArchiver.archivedData(withRootObject:json)
+        self.setData(strData: encodedData, strKey: key)
+    }
+    func saveJSON(json: JSON, key:String){
+        if let jsonString = json.rawString() {
+            UserDefaults.standard.setValue(jsonString, forKey: key)
+        }
+    }
+    
+    func getJSON(_ key: String)-> JSON? {
+        var p = ""
+        if let result = UserDefaults.standard.string(forKey: key) {
+            p = result
+        }
+        if p != "" {
+            if let json = p.data(using: String.Encoding.utf8, allowLossyConversion: false) {
+                do {
+                    return try JSON(data: json)
+                } catch {
+                    return nil
+                }
+            } else {
+                return nil
+            }
+        } else {
+            return nil
+        }
+    }
+//    public func loadJSON(strKey:String) -> JSON {
+//        let defaults = UserDefaults.standard
+//        return JSON (parseJSON: (defaults.value(forKey: strKey) as! String))
+////        return JSON.parse(defaults.valueForKey(strKey) as! String))
+//        // JSON from string must be initialized using .parse()
+//    }
+    // get color from string
+    
+    override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
+        
+        if (toInterfaceOrientation.isLandscape) {
+            NSLog("Landscape");
+        }
+        else {
+            NSLog("Portrait");
+        }
+    }
+    func getColorFromString(pString:String) -> UIColor {
+        
+        var strokeColorInString = pString
+        strokeColorInString = strokeColorInString.replacingOccurrences(of: "rgba(", with: "")
+        strokeColorInString = strokeColorInString.replacingOccurrences(of: ")", with: "")
+        
+        let strokesArray = strokeColorInString.components(separatedBy: ",")
+        
+        return UIColor(red:  CGFloat(Double(strokesArray[0])!/255.0), green: CGFloat(Double(strokesArray[1])!/255.0), blue: CGFloat(Double(strokesArray[2])!/255.0), alpha:  CGFloat(Double(strokesArray[3])!))
+    }
+//    @objc func networkStatusChanged(_ notification: Notification) {
+//
+//        let status = Reach().connectionStatus()
+//        switch status {
+//        case .unknown, .offline:
+//            print("Not connected")
+//            APP.open_count = 2
+////           self.processingAlert?.show(nil, hidden: nil)
+//         case .online(.wwan):
+//            print("Connected via WWAN")
+//            self.processingAlert?.hideAlert({ () -> () in
+//                if self.APP.open_count == 2 {
+//                    self.APP.open_count = 0
+////                    self.showAlert(message: Language.Common.Connected, type: AlertType.success, navBar: false)
+//                }
+//                    })
+//        case .online(.wiFi):
+//            print("Connected via WiFi")
+//
+//            self.processingAlert?.hideAlert({ () -> () in
+//                if self.APP.open_count == 2 {
+//                    self.APP.open_count = 0
+////                    self.showAlert(message: Language.Common.Connected, type: AlertType.success, navBar: false)
+//                }
+//                    })
+//        }
+//    }
+    
+
+    
+    
+    func heightForView(text:String?, width:CGFloat , Label : UILabel) -> Int{
+        let hValue = CGFloat.greatestFiniteMagnitude
+        Label.frame.size.height = hValue
+        Label.numberOfLines = 0
+        Label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        Label.text = text
+        Label.sizeToFit()
+        //if Int(Label.frame.height) < LABEL_HEIGHT || Int(Label.frame.height)  <= 41 {
+        
+        if Int(Label.frame.height) < CUSTOM_TEXTFIELD_HEIGHT  {
+            return CUSTOM_TEXTFIELD_HEIGHT
+        }
+        else{
+            return Int(Label.frame.height)
+        }
+    }
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+}
---
+++ Utils.swift
@@ -0,0 +1,266 @@
+//
+//  Utils.swift
+//  iBeacon
+//
+//  Created by Gunjan on 02/09/19.
+//  Copyright © 2019 Gunjan. All rights reserved.
+//
+
+import UIKit
+import JQProgressHUD
+import Foundation
+import SwiftyJSON
+
+
+class Utils: NSObject {
+
+    static func getAlertController(title : String, message : String ,style: UIAlertController.Style) ->  UIAlertController {
+        
+        let alertVc = UIAlertController.init(title: title, message: message, preferredStyle: style)
+        alertVc.view.tintColor = colorPrimary
+        let attrs = [kCTFontAttributeName as NSAttributedString.Key:UIFont.boldSystemFont(ofSize: 18.0),kCTForegroundColorAttributeName as NSAttributedString.Key:colorPrimary]
+        let hogen = NSMutableAttributedString.init(string: title, attributes: attrs)
+        alertVc.setValue(hogen,forKey: "attributedTitle")
+        let attrsM  = [kCTFontAttributeName as NSAttributedString.Key:UIFont.systemFont(ofSize: 15.0) ,kCTForegroundColorAttributeName as NSAttributedString.Key:UIColor.black]
+        let hogenMessage = NSMutableAttributedString.init(string: message, attributes: attrsM)
+        alertVc.setValue(hogenMessage,forKey: "attributedMessage")
+        return alertVc
+    }
+
+    static  func showAlert(targetVC: UIViewController, title: String, message: String ,style: UIAlertController.Style){
+        DispatchQueue.main.async(execute: {
+            let alert = getAlertController(title : title, message : message, style: style)
+            alert.addAction((UIAlertAction(title: "OK", style: .default, handler: {(action) -> Void in
+            })))
+
+            targetVC.present(alert, animated: true, completion: nil)
+        })
+    }
+    static func getDate(complete date:String)-> String {
+        if (date == ""){
+            return date
+        }
+        let splitdate : [Substring] = date.split(separator: ".")
+        let subString = splitdate.first!
+        let newDate = String(subString)
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "yyyy-MM-dd"
+
+        if let date1 = dateFormatterGet.date(from:newDate ){
+            return dateFormatterPrint.string(from:date1)
+        }
+        else {
+            print("There was an error decoding the string")
+        }
+        return date
+    }
+    static func attributedString(str : String) -> NSAttributedString? {
+        let attributes : [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14.0),
+            NSAttributedString.Key.foregroundColor : colorPrimaryDark,
+            NSAttributedString.Key.underlineStyle : NSUnderlineStyle.single.rawValue
+        ]
+        let attributedString = NSAttributedString(string: str, attributes: attributes)
+        return attributedString
+    }
+    static func mulitplelinebutton(strText :NSString , btn :UIButton){
+        
+        
+        let fulltext : String = strText as String
+        //getting the range to separate the button title strings
+        let newlineRange: NSRange = strText.range(of: "\n")
+        
+        //getting both substrings
+        var substring1 = ""
+        var substring2 = ""
+        
+        if(newlineRange.location != NSNotFound) {
+            substring1 = strText.substring(to: newlineRange.location)
+            substring2 = strText.substring(from: newlineRange.location)
+        }
+        
+        //assigning diffrent fonts to both substrings
+        let font1: UIFont = UIFont .boldSystemFont(ofSize:SMALL_BUTTON_FONT_SIZE)
+        let attributes1: [NSAttributedString.Key: Any] = [
+            .font: font1,
+            .foregroundColor: colorSubHeading_76
+        ]
+        let attrString1 = NSMutableAttributedString(string: substring1, attributes: attributes1)
+        
+        
+        let font2: UIFont = UIFont .systemFont(ofSize:  SUB_LABEL_DESC_FONT_SIZE)
+        let attributes2: [NSAttributedString.Key: Any] = [
+            .font: font2,
+            .foregroundColor: colorSubSubHeading_94
+        ]
+        
+        let attrString2 = NSMutableAttributedString(string: substring2, attributes: attributes2)
+        
+        attrString1.append(attrString2)
+
+        //assigning the resultant attributed strings to the button
+        btn.setAttributedTitle(attrString1, for: [])
+    
+    }
+//    static func showLoading(title : String)  {
+//        JQProgressHUDTool.jq_showCustomHUD( msg: title)
+//    }
+//    static func hideLoading()  {
+//        JQProgressHUDTool.jq_hideHUD()
+//    }
+    static func mimeType(for data: Data) -> String {
+        
+        var b: UInt8 = 0
+        data.copyBytes(to: &b, count: 1)
+        
+        switch b {
+        case 0xFF:
+            return "image/jpeg"
+        case 0x89:
+            return "image/png"
+        case 0x47:
+            return "image/gif"
+        case 0x4D, 0x49:
+            return "image/tiff"
+        case 0x25:
+            return "application/pdf"
+        case 0xD0:
+            return "application/vnd"
+        case 0x46:
+            return "text/plain"
+        default:
+            return "application/octet-stream"
+        }
+    }
+//    static func stringFromJson(object: [[String : Any]]) -> String{
+//        let newjson = JSON(object)
+//        let sjod = newjson.rawString()
+//        return sjod!
+////        let jsonData = try? JSONSerialization.data(withJSONObject: object, options: [])
+////        let jsonString = String(data: jsonData!, encoding: .utf8)
+////        return jsonString!
+//    }
+//    static func jsonObject(jsonString : String) -> [[String : Any]] {
+//        let jsonData = jsonString.data(using: .utf8)
+//        let dictionary = try? JSONSerialization.jsonObject(with: jsonData!, options:  [])
+//        return dictionary as! [[String : Any]]
+//    }
+    static func dataFromJson(object: [String : Any]) -> Data {
+        let jsonData = try? JSONSerialization.data(withJSONObject: object, options: [])
+        if(jsonData == nil){
+            return Data()
+        }
+        return jsonData!
+    }
+    static func jsonFromData(data : Data)-> [String : Any] {
+        let dictionary = try? JSONSerialization.jsonObject(with: data, options: [])
+        if(dictionary == nil){
+            return [:]
+        }
+        return dictionary as! [String : Any]
+    }
+    static func stringFromJson(object: [String : Any]) -> String{
+        let jsonData = try? JSONSerialization.data(withJSONObject: object, options: [])
+        let jsonString = String(data: jsonData!, encoding: .utf8)
+        return jsonString!
+    }
+    static func jsonObject(jsonString : String) -> [String : Any] {
+        let jsonData = jsonString.data(using: .utf8)
+        let dictionary = try? JSONSerialization.jsonObject(with: jsonData!, options:  [])
+        return dictionary! as! [String : Any]
+    }
+   static func getTextfield(view: UIView) -> [UITextField] {
+        var results = [UITextField]()
+        for subview in view.subviews as [UIView] {
+            if let textField = subview as? UITextField {
+                if textField.text == "" {
+//                    ParentClass.shared.checkTextfield = 1
+                }else{
+                    results += [textField]
+                }
+            } else {
+                results += getTextfield(view: subview)
+            }
+        }
+        return results
+    }
+    static func cStoreGetTextfield(view: UIView) -> [UITextField] {
+        var results = [UITextField]()
+        for subview in view.subviews as [UIView] {
+            if let textField = subview as? UITextField {
+                if textField.text == "" && (textField.tag == 112 || textField.tag == 114  || textField.tag == 116 || textField.tag == 118  || textField.tag == 128  || textField.tag == 130  || textField.tag == 132) {
+//                    ParentClass.shared.checkTextfield = 1
+                }else{
+                    results += [textField]
+                }
+            } else {
+                results += cStoreGetTextfield(view: subview)
+            }
+        }
+        return results
+    }
+//    && (textField.tag != 101 || textField.tag != 102 || textField.tag != 103 || textField.tag != 104 || textField.tag != 105 || textField.tag != 106 || textField.tag != 107 || textField.tag != 108 || textField.tag != 109 || textField.tag != 110 || textField.tag != 111 || textField.tag != 112 || textField.tag != 113)
+    static func registerTextfield(view: UIView) -> [UITextField] {
+        var results = [UITextField]()
+        for subview in view.subviews as [UIView] {
+            if let textField = subview as? UITextField {
+                if  textField.text == ""  && textField.tag != 0 {
+//                    ParentClass.shared.checkTextfield = 1
+                }else{
+                    results += [textField]
+                }
+            } else {
+                results += registerTextfield(view: subview)
+            }
+        }
+        return results
+    }
+    
+    static func keyboardTextfield(view: UIView) -> [UITextField] {
+        var results = [UITextField]()
+        for subview in view.subviews as [UIView] {
+            if let textField = subview as? UITextField {
+                    results += [textField]
+            } else {
+                results += registerTextfield(view: subview)
+            }
+        }
+        return results
+    }
+    
+    static func stringFromJson(object: [JSON]) -> String{
+        let newjson = JSON(object)
+        let sjod = newjson.rawString()
+        return sjod!
+        //        let jsonData = try? JSONSerialization.data(withJSONObject: object, options: [])
+        //        let jsonString = String(data: jsonData!, encoding: .utf8)
+        //        return jsonString!
+    }
+//    static func jsonObject(jsonString : String) -> JSON {
+//        let jsonData = jsonString.data(using: .utf8)
+//        if (jsonData != nil) {
+//            let finalJSON = try? JSON(data: jsonData!)
+//            return finalJSON!
+//        }
+//        else{
+//            return JSON()
+//        }
+//    }
+}
+
+extension Array {
+  func sliced(by dateComponents: Set<Calendar.Component>, for key: KeyPath<Element, Date>) -> [Date: [Element]] {
+    let initial: [Date: [Element]] = [:]
+    let groupedByDateComponents = reduce(into: initial) { acc, cur in
+      let components = Calendar.current.dateComponents(dateComponents, from: cur[keyPath: key])
+      let date = Calendar.current.date(from: components)!
+      let existing = acc[date] ?? []
+      acc[date] = existing + [cur]
+    }
+
+    return groupedByDateComponents
+  }
+}
